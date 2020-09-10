2b71edbe4c0a80c980664c5b4b7a116097e2d3c1
Marlom Ramos <marlomramost@gmail.com>
2020-09-10 14:04:00 -0700

    Prueba de aprendizaje de git

--- prueba.js
+++ prueba.js
@@ -1,1 +1,16 @@
-console.log("hola mundo 2")
+var miobjeto={
+    valor1:10,
+    valor2:11,
+    valor3:true,
+    mifunction:(a,b)=>a+b*1000,
+    objeto2:{
+        valorobj2_1:2,
+        valorobj2_2:222
+    },
+    Objeto3:434,
+    fecha: new Date()
+
+}
+
+console.log(miobjeto.fecha.getMilliseconds())
+console.log(miobjeto.mifunction(2,3))
